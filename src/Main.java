48f7d5169b8d136fcb5c46f2c4660afdd3a52222
SGDoubler <doublesn@mail.uc.edu>
2023-06-11 13:19:22 -0400

    2

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int springCosts = 340;
+        int summerCosts = 75;
+        int fallCosts = 140;
+        int winterCosts = 160;
+
+        int totalYearlyCosts = springCosts + summerCosts + fallCosts + winterCosts;
+
+        System.out.println("The total yearly maintance costs of Spring $" + springCosts + ", Summer $" + summerCosts +
+                ", Fall $" + fallCosts + ", and Winter $" + winterCosts + ", is $" + totalYearlyCosts);
+
+
+
+    }
+}
